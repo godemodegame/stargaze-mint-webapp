a938ba111297cb1151cc4182044942b80cd37606
Kirill Kirilenko <godemodegame@icloud.com>
2024-03-02 11:04:46 +0200

    always expand webapp

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
 import MintPage from './pages/MintPage/MintPage';
+import MintList from './pages/MintList/MintList';
+import { useEffect } from 'react';
+import WebApp from '@twa-dev/sdk';
 
 function App() {
+  useEffect(() => {
+    WebApp.expand();
+  }, []);
+
   return (
    <>
     <Routes>
       <Route path="*" element={<MintPage />} />
       <Route path="/mint" element={<MintPage />} />
+      <Route path="/list" element={<MintList />} />
     </Routes>
    </>
   );
